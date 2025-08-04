56c934ed92cbacc7aec768b226aec236aeeb4421
Danil <stonewidg@mail.ru>
2025-08-04 17:14:59 +0300

    new file:   .venv/Lib/site-packages/templates-0.0.5.dist-info/INSTALLER 	new file:   .venv/Lib/site-packages/templates-0.0.5.dist-info/METADATA 	new file:   .venv/Lib/site-packages/templates-0.0.5.dist-info/RECORD 	new file:   .venv/Lib/site-packages/templates-0.0.5.dist-info/REQUESTED 	new file:   .venv/Lib/site-packages/templates-0.0.5.dist-info/WHEEL 	new file:   .venv/Lib/site-packages/templates-0.0.5.dist-info/top_level.txt 	new file:   .venv/Lib/site-packages/templates/__init__.py 	new file:   .venv/Lib/site-packages/templates/base.py 	new file:   .venv/Lib/site-packages/templates/bootstrap/__init__.py 	new file:   .venv/Lib/site-packages/templates/bootstrap/base.py 	new file:   .venv/Lib/site-packages/templates/bootstrap/bootstrap.min.css 	new file:   .venv/Lib/site-packages/templates/bootstrap/bootstrap.min.js 	new file:   .venv/Lib/site-packages/templates/bootstrap/jquery-3.2.1.slim.min.js 	new file:   .venv/Lib/site-packages/templates/bootstrap/jumbotron.py 	new file:   .venv/Lib/site-packages/templates/bootstrap/popper.min.js 	new file:   .venv/Lib/site-packages/templates/bootstrap/starter.py 	new file:   .venv/Lib/site-packages/templates/demo.py 	modified:   __pycache__/main.cpython-313.pyc 	new file:   app/static/about.html 	new file:   app/static/otherpg.html 	new file:   app/static/templates 	modified:   main.py 	modified:   pyproject.toml 	modified:   uv.lock

--- main.py
+++ main.py
@@ -1,38 +1,16 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 import uvicorn
-from utils import feedback_logger_to_file, validate_form_data
-from db import db_con
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(title="Форма связи")
-app.mount("/static", StaticFiles(directory='./static'), name='static')
-templates = Jinja2Templates('./templates')
 
-@app.get('/')
-def root(request: Request):
-    return templates(request=request, name='index.html')
+app.mount("/index", StaticFiles(directory='./app/static'))
+templates = Jinja2Templates("./app/static")
 
-@app.post('/feedback_form')
-async def test_post(request: Request):
-    form_data = await request.json()
-    name = form_data['name']
-    email = form_data['email']
-    phone = form_data['phone']
+@app.get("/")
+async def get_root(request: Request):
+    return templates.TemplateResponse(request=request, name="about.html")
 
-    result_validate_data = validate_form_data(name, email, phone)
-
-    if not result_validate_data['name']:
-        return "Некоректное имя"
-    elif not result_validate_data['email']:
-        return"Некоректный email" 
-    elif not result_validate_data['phone']:
-        return "Некоректный номер телефона"
-    else:
-        str_for_logger = f'имя: {name}, Почта {email}, Номер телефона: {phone}'
-        feedback_logger_to_file(str_for_logger)
-        db_con.add_data(name, email, phone)
-        return templates.TemplateResponse(request=request, name='success_form_status.html')
-
-if __name__ == '__main__':
-    uvicorn.run(app=app, port=80, host='192.168.88.59')
+if __name__ == "__main__":
+    uvicorn.run(app, host="192.168.88.59", port=80)
