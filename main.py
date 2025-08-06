7b82b9f82e68afcf5e867a79d9b34f3d0ff4d706
Danil <stonewidg@mail.ru>
2025-08-06 17:17:23 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   app/static/about.html
    	new file:   app/static/me.html
    	deleted:    app/static/otherpg.html
    	new file:   app/static/reg.html
    	new file:   app/static/vhod.html
    	new file:   app/static/zak.html
    	new file:   image/memar.jpg
    	modified:   main.py

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, responses
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI(title="Форма связи")
-
-app.mount("/index", StaticFiles(directory='./app/static'))
 templates = Jinja2Templates("./app/static")
 
 @app.get("/")
 async def get_root(request: Request):
     return templates.TemplateResponse(request=request, name="about.html")
+
+@app.get("/me")
+async def get_otherpg(request: Request):
+    return templates.TemplateResponse(request=request, name="me.html")
+
+@app.get("/vhod")
+async def get_otherpg(request: Request):
+    return templates.TemplateResponse(request=request, name="vhod.html")
+
+@app.get("/reg")
+async def get_otherpg(request: Request):
+    return templates.TemplateResponse(request=request, name="reg.html")
+
+@app.get("/zak")
+async def get_otherpg(request: Request):
+    return templates.TemplateResponse(request=request, name="zak.html")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="192.168.88.59", port=80)
