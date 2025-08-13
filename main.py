7768891befb35abaf411a04b361dca6587a00f7c
Danil <stonewidg@mail.ru>
2025-08-13 16:03:31 +0300

    modified:   app/static/about.html 	modified:   main.py

--- main.py
+++ main.py
@@ -26,9 +26,5 @@ async def get_otherpg(request: Request):
 async def get_otherpg(request: Request):
     return templates.TemplateResponse(request=request, name="zak.html")
 
-@app.get("/mef.png")
-async def get_otherpg(request: Request):
-    return templates.TemplateResponse(request=request, name="mef.png")
-
 if __name__ == "__main__":
     uvicorn.run(app, host="192.168.88.59", port=80)
