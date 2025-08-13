a21321b57d1d0d115124fc989d9caa6c645f138f
Danil <stonewidg@mail.ru>
2025-08-13 15:42:54 +0300

    modified:   app/static/about.html 	modified:   app/static/me.html 	modified:   main.py 	renamed:    image/memar.jpg -> mef.jpg

--- main.py
+++ main.py
@@ -26,5 +26,9 @@ async def get_otherpg(request: Request):
 async def get_otherpg(request: Request):
     return templates.TemplateResponse(request=request, name="zak.html")
 
+@app.get("/logo")
+async def get_otherpg(request: Request):
+    return templates.TemplateResponse(request=request, name="mef.jpg")
+
 if __name__ == "__main__":
     uvicorn.run(app, host="192.168.88.59", port=80)
